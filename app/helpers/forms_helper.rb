0c33c91c450e44a72f78bfac12ba95b034340b40
ademizinep <ademirjunior.eng@gmail.com>
2024-08-28 11:41:58 -0300

    Add forms helper

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,2 +1,9 @@
 module FormsHelper
+  def self.random_exam_name
+    substantives = ['Exame', 'Teste', 'Análise', 'Avaliação']
+    adjectives = ['Olho', 'Íris', 'Pupila', 'Córnea', 'Retina', 'Cristalino', 'Conjuntiva', 'Esclera', 'Nervo Óptico', 'Câmara Anterior', 'Câmara Posterior', 'Vítreo']
+    sides = ['Direito', 'Esquerdo']
+
+    "#{substantives.sample} #{adjectives.sample} #{sides.sample}"
+  end
 end
